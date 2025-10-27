1bf402b0f89cfd066d52ada9a0b3f616a0aa96c4
vsevolod6 <ZakurnaevVD23@st.ithub.ru>
2025-10-28 00:02:10 +0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Checklist from './Checklist';
+import './Checklist.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Checklist />
+    </div>
+  );
+}
+
+export default App;
